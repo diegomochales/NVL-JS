0fbc6ba970f56eb8fd1ec4e98a8500f3ff229359
Diego Mochales <mochalesmattediego@gmail.com>
2020-02-10 16:18:34 +0100

    añadimos practica

---
+++ P9b/Practica-JS-4.js
@@ -0,0 +1,2 @@
+var conciertos = [["Lunes", "Madrid"], ["Martes", "Sevilla"], ["Miercoles", "Barcelona"], ["Jueves", "Valencia"], ["Viernes", "Zaragoza"], ["Sabado", "Bilbao"], ["Domingo", "Donosti"]];
+alert(conciertos)
