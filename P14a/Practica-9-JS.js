62f28734238456c125d2aab6eb6d654b38bd71b1
Diego Mochales <mochalesmattediego@gmail.com>
2020-02-18 17:56:50 +0100

    añadimos practica sin terminar

---
+++ P14a/Practica-9-JS.js
@@ -0,0 +1,113 @@
+var pelis = [
+    {
+        "Nombre": "El señor de los anillos",
+        "Genero": "Acción"
+    },
+    {
+        "Nombre": "Regreso al futuro",
+        "Genero": "Acción"
+    },
+    {
+        "Nombre": "Los increibles 2",
+        "Genero": "Animación"
+    },
+    {
+        "Nombre": "Star wars",
+        "Genero": "Acción"
+    },
+    {
+        "Nombre": "Son como niños",
+        "Genero": "Comedia"
+    },
+    {
+        "Nombre": "Ratatouille",
+        "Genero": "Animación"
+    },
+    {
+        "Nombre": "Up",
+        "Genero": "Animación"
+    },
+    {
+        "Nombre": "Marley y yo",
+        "Genero": "Drama"
+    },
+    {
+        "Nombre": "Indiana jones",
+        "Genero": "Acción"
+    },
+    {
+        "Nombre": "Deadpool",
+        "Genero": "Comedia"
+    },
+    {
+        "Nombre": "Rapidos y furiosos",
+        "Genero": "Acción"
+    },
+    {
+        "Nombre": "Los vengadores",
+        "Genero": "Acción"
+    },
+    {
+        "Nombre": "Superbad",
+        "Genero": "Comedia"
+    },
+    {
+        "Nombre": "007",
+        "Genero": "Acción"
+    },
+    {
+        "Nombre": "Titanic",
+        "Genero": "Drama"
+    },
+    {
+        "Nombre": "La mascara",
+        "Genero": "Comedia"
+    },
+    {
+        "Nombre": "Inside out",
+        "Genero": "Animación"
+    },
+    {
+        "Nombre": "Capitan America",
+        "Genero": "Acción"
+    },
+    {
+        "Nombre": "Intouchables",
+        "Genero": "Drama"
+    },
+    {
+        "Nombre": "Cazafantasmas",
+        "Genero": "Comedia"
+    },
+    {
+        "Nombre": "Hachiko",
+        "Genero": "Drama"
+    },
+    {
+        "Nombre": "Hotel transylvania",
+        "Genero": "Animación"
+    },
+    {
+        "Nombre": "Toy story",
+        "Genero": "Animación"
+    },
+    {
+        "Nombre": "American pie",
+        "Genero": "Comedia"
+    },
+    {
+        "Nombre": "El rey león",
+        "Genero": "Animación"
+    }
+]
+
+pelisStr = JSON.stringify(pelis);
+
+pelisStr.Genero.Comedia 
+document.write(pelisStr);
+
+
+
+
+
+
